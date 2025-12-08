d157656149c68f2de2977102b04264f95082589f
Akash-V-P <akashvp.dev@gmail.com>
2025-12-08 18:37:35 +0530

    add indexing/unique constraints to like model for video, comment, tweet, and book

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -28,4 +28,11 @@ const likeSchema = new Schema(
     }
 );
 
+//for adding unique documents so that user cannot like same video/comment/tweet/book more than once
+likeSchema.index({ video: 1, likedBy: 1 }, { unique: true });
+likeSchema.index({ comment: 1, likedBy: 1 }, { unique: true });
+likeSchema.index({ tweet: 1, likedBy: 1 }, { unique: true });
+likeSchema.index({ book: 1, likedBy: 1 }, { unique: true });
+
+
 export const Likes = mongoose.model("Likes", likeSchema);
